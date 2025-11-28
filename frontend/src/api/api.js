eba9e636f3826e34544e48481069c81b8c62ab66
Talytha Coimbra <talythagusmao@gmail.com>
2025-11-27 16:03:38 -0800

    Change API base URL to production endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 // backend local
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://silent-auction-psw5.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
 });
+
